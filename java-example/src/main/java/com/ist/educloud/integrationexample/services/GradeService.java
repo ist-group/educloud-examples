56976cbc5b39cb7604787cf1faa43ce684af7fa9
Anders Matson <Anders.Matson@ist.com>
2022-01-24 11:14:47 +0100

    Fix

---
+++ java-example/src/main/java/com/ist/educloud/integrationexample/services/GradeService.java
@@ -0,0 +1,10 @@
+package com.ist.educloud.integrationexample.services;
+
+import java.util.List;
+
+import com.ist.educloud.integrationexample.dtos.GradeDTO;
+
+public interface GradeService {
+
+    List<GradeDTO> getAllGrades();
+}
